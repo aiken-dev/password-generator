b06893e8ca85faa7bce5903e1b820ff6b4cc4094
aiken-dev <89868342+aiken-dev@users.noreply.github.com>
2024-12-05 09:04:01 +0000

    password generator

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
+import django
+import string  
+import random
+letters = list(string.ascii_letters)
+digits = list(string.digits)
+punctuation = ["!", "@", "#", "$", "%", "^", "&", "*"]
+
+characters = letters + digits + punctuation
+
+
+howManyPasswords = int(input("Enter how many passwords to generate?: "))
+passwordsFileName = input("Name the file to output passwords (default: passwords.txt): ")
+if passwordsFileName == "":
+    passwordsFile = passwordsFile = open("passwords.txt", "a")
+else:
+    passwordsFile = open(f"{passwordsFileName}.txt", "a")
+
+for r in range(howManyPasswords):
+    password = ""
+    while len(password) != 14:
+        for i in range(len(characters)):
+            randomChar = random.randint(1, len(characters))
+
+            if i == randomChar:
+                password = password + characters[i]
+                
+
+    print(password)
+    passwordsFile.write(f'{password}\n')
+
+
+passwordsFile.close()
+
 
