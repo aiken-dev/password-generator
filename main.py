c4cc47c91a95a15a4ec57844e9c8e6275093c4c6
aiken-dev <89868342+aiken-dev@users.noreply.github.com>
2024-12-10 12:29:24 +0000

    cleaned up, need to fix if passwordLength input = ""

--- main.py
+++ main.py
@@ -9,30 +9,51 @@ characters = letters + digits + punctuation
 
 
 
-howManyPasswords = int(input("Enter how many passwords to generate?: "))
-passwordsFileName = input("Name the file to output passwords (default: passwords.txt): ")
 
 
-if passwordsFileName == "":
-    passwordsFile = passwordsFile = open("passwords.txt", "a")
-else:
-    passwordsFile = open(f"{passwordsFileName}.txt", "a")
 
-def generatePassword():
+
+def generatePassword(howManyPasswords, passwordLength, passwordsFileName):
     password = ""
-    while any(ele in password for ele in letters) == False or any(ele in password for ele in digits) == False or any(ele in password for ele in punctuation) == False:
-        for i in range(14):
+    while any(ele in password for ele in letters) == False and any(ele in password for ele in digits) == False and any(ele in password for ele in punctuation) == False:
+        for i in range(passwordLength):
             password = password + random.choice(characters)
+            
 
     
         
     return password
 
+
+howManyPasswords = input("Enter how many passwords to generate? (default: 12): ")
+passwordsFileName = input("Name the file to output passwords (default: passwords.txt): ")
+passwordLength = input("How long should each password be? (default: 14): ")
+
+
+if howManyPasswords == "":
+    howManyPasswords = 12
+else:
+    howManyPasswords = int(howManyPasswords)
+    
+if passwordsFileName == "":
+    passwordsFile = passwordsFile = open("passwords.txt", "a")
+else:
+
+    passwordsFile = open(f"{passwordsFileName}.txt", "a")
+
+if passwordLength == "":
+    passwordLength == 12
+else:
+    passwordLength = int(passwordLength)
+
 for i in range(howManyPasswords):
-    print(generatePassword())
-    passwordsFile.write(f'{generatePassword()}\n')
+    print(generatePassword(howManyPasswords, passwordLength, passwordsFileName))
+    passwordsFile.write(f'{generatePassword(howManyPasswords, passwordLength, passwordsFileName)}\n')
 
-
+print(f"Generated {howManyPasswords} passwords, each {passwordLength} characters long and stored them in file", f"{passwordsFileName}.txt")
 passwordsFile.close()
+
+
+
 
 
